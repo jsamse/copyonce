fe5bfb5b402fb5c0ebb39e956b9eeb93b4a7935d
Jonas Samuelsson <jonaspetter@gmail.com>
2024-09-29 16:07:08 +0200

    Replace types with include/exclude

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { sleep } from "bun";
 import { Database } from "bun:sqlite";
 import { readdir, stat, mkdir, copyFile, watch } from "node:fs/promises";
-import { sep } from "node:path";
+import { sep, dirname } from "node:path";
 import { parseArgs } from "util";
 
 class File {
@@ -14,11 +14,12 @@ const { values, positionals } = parseArgs({
     options: {
         interval: {
             type: 'string',
-            short: 'i',
         },
-        types: {
+        include: {
             type: 'string',
-            short: 't',
+        },
+        exclude: {
+            type: 'string',
         },
         help: {
             type: 'boolean',
@@ -30,10 +31,11 @@ const { values, positionals } = parseArgs({
 });
 
 if (values.help || positionals.length < 5) {
-    console.log("Usage: copyonce [--interval 60 --types pdf,gif] <db> <source> <destination>");
+    console.log("Usage: copyonce [--interval 60 --include regexp --exclude regexp] <db> <source> <destination>");
     console.log("");
     console.log("if --interval is set, the program wait that number of seconds between copying again");
-    console.log("--types is a comma separated list of file extensions to copy");
+    console.log("--include includes only files that match the regular expression, includes everything if not set");
+    console.log("--exclude overrides --include");
     console.log("<db> is the path to the sqlite database to store file information");
     console.log("<src> is the source directory to copy from");
     console.log("<dest> is the destination directory to copy to");
@@ -45,9 +47,14 @@ if (values.interval) {
     interval = parseInt(values.interval);
 }
 
-let types: string[] = [];
-if (values.types) {
-    types = values.types.split(',');
+let include: RegExp | undefined = undefined;
+if (values.include) {
+    include = new RegExp(values.include);
+}
+
+let exclude: RegExp | undefined = undefined;
+if (values.exclude) {
+    exclude = new RegExp(values.exclude);
 }
 
 const db = new Database(positionals[2], { strict: true });
@@ -63,23 +70,16 @@ db.run("CREATE TABLE IF NOT EXISTS files (path TEXT NOT NULL PRIMARY KEY, modifi
 const src = positionals[3].endsWith(sep) ? positionals[3].slice(0, -1) : positionals[3];
 const dest = positionals[4].endsWith(sep) ? positionals[4].slice(0, -1) : positionals[4];
 
-
 while (true) {
     const files = (await readdir(src, { recursive: true }));
 
     for (const file of files) {
         const stats = await stat(`${src}${sep}${file}`);
         const row = db.query("SELECT modified, size FROM files WHERE path = ?").as(File).get(file);
-        if (stats.isDirectory()) {
-            if (!row) {
-                console.log(`Copying directory: ${file}`);
-                mkdir(`${dest}${sep}${file}`, { recursive: true });
-                db.run("INSERT INTO files (path, modified, size) VALUES (?, ?, ?)", [file, stats.mtimeMs, 0]);
-            }
-        }
         if (stats.isFile()) {
-            if ((!row || row.modified !== stats.mtimeMs || row.size !== stats.size) && (types.length === 0 || types.includes(file.split('.').pop() || ''))) {
+            if ((!row || row.modified !== stats.mtimeMs || row.size !== stats.size) && (!include || include.test(file)) && (!exclude || !exclude.test(file))) {
                 console.log(`Copying file: ${file}`);
+                mkdir(`${dest}${sep}${dirname(file)}`, { recursive: true });
                 await copyFile(`${src}${sep}${file}`, `${dest}${sep}${file}`);
                 db.run("INSERT OR REPLACE INTO files (path, modified, size) VALUES (?, ?, ?)", [file, stats.mtimeMs, stats.size]);
             }
@@ -87,6 +87,7 @@ while (true) {
     }
 
     if (interval === 0) {
+        db.close();
         process.exit();
     }
     await sleep(interval * 1000);
